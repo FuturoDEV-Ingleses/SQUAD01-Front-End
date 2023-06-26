402f3e75ea8121224219b5dc0edfc6d767e2b44f
VivianeOliveira <viviany-oliveira@outlook.com>
2023-06-26 20:01:41 -0300

    Atualização

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getData, updateData, deleteData } from "../../utils/index";
 import { Cards, Container } from "../../components";
 
 export default function Dashboard() {
   const [filter, setFilter] = useState("all");
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await getData("dashboard");
+      setData(response);
+      setLoading(false);
+    } catch (error) {
+      console.error("Erro ao buscar dados do dashboard:", error);
+    }
+  };
 
   return (
     <Container title="Dashboard">
-      <Cards setFilter={setFilter} />
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
+        <Cards data={data} setFilter={setFilter} />
+      )}
     </Container>
   );
 }
